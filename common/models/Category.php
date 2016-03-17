35875e4ae1f0de01f02a888dc7c1e212b00b01c2
yun.song <mingzhehao@github.com>
2016-03-17 16:58:49 +0800

    crud 创建category分类

---
+++ common/models/Category.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'status'], 'required'],
+            [['status', 'created_at', 'updated_at'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['name'], 'unique']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => '分类名称',
+            'status' => '分类状态',
+            'created_at' => '创建时间',
+            'updated_at' => '更新时间',
+        ];
+    }
+}
