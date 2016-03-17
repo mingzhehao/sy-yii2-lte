d92c83276ee1d86e7b5cbe5592f10cd639076fd0
yun.song <mingzhehao@github.com>
2016-03-17 15:03:52 +0800

    开启gii ，开启debug 在默认设置环境

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -14,11 +14,13 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['192.168.56.*', '127.0.0.1', '::1']
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => ['192.168.56.*', '127.0.0.1', '::1']
     ];
 }
 
