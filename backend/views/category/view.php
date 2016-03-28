d2b1082744e5c1854ad351a6cd3bf10572b2c8e0
yun.song <mingzhehao@github.com>
2016-03-28 19:12:44 +0800

    去除存在的h1大标题

--- backend/views/category/view.php
+++ backend/views/category/view.php
@@ -12,8 +12,6 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="category-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
         <?= Html::a('更新', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('删除', ['delete', 'id' => $model->id], [
