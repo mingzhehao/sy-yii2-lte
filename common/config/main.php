e539354d041486897ec9263a353221e6321af3e1
yun.song <mingzhehao@github.com>
2016-04-11 16:57:34 +0800

    添加系统信息功能，可以实时查看系统相关信息

--- common/config/main.php
+++ common/config/main.php
@@ -21,6 +21,40 @@ return [
                  ],
              ],
          ],
+        'i18n' => [
+            'translations' => [
+                'app'=>[
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath'=>'@common/messages',
+                ],
+                '*'=> [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath'=>'@common/messages',
+                    'fileMap'=>[
+                        'common'=>'common.php',
+                        'backend'=>'backend.php',
+                        'frontend'=>'frontend.php',
+                    ],
+                    'on missingTranslation' => ['\backend\modules\i18n\Module', 'missingTranslation']
+                ],
+            ],
+        ],
+        //'log' => [
+        //    'traceLevel' => YII_DEBUG ? 3 : 0,
+        //    'targets' => [
+        //        'db'=>[
+        //            'class' => 'yii\log\DbTarget',
+        //            'levels' => ['error', 'warning'],
+        //            'except'=>['yii\web\HttpException:*', 'yii\i18n\I18N\*'],
+        //            'prefix'=>function () {
+        //                $url = !Yii::$app->request->isConsoleRequest ? Yii::$app->request->getUrl() : null;
+        //                return sprintf('[%s][%s]', Yii::$app->id, $url);
+        //            },
+        //            'logVars'=>[],
+        //            'logTable'=>'{{%system_log}}'
+        //        ]
+        //    ],
+        //],
         'elasticsearch' => [
             'class' => 'yii\elasticsearch\Connection',
             'nodes' => [
