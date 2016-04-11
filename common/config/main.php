e40512c1f30b1f93326e9c08bbda7c5b13ac2509
yun.song <mingzhehao@github.com>
2016-04-11 17:08:45 +0800

    日志功能添加，log信息写入数据库，便于线上实时查看，无需登录服务器

--- common/config/main.php
+++ common/config/main.php
@@ -39,22 +39,22 @@ return [
                 ],
             ],
         ],
-        //'log' => [
-        //    'traceLevel' => YII_DEBUG ? 3 : 0,
-        //    'targets' => [
-        //        'db'=>[
-        //            'class' => 'yii\log\DbTarget',
-        //            'levels' => ['error', 'warning'],
-        //            'except'=>['yii\web\HttpException:*', 'yii\i18n\I18N\*'],
-        //            'prefix'=>function () {
-        //                $url = !Yii::$app->request->isConsoleRequest ? Yii::$app->request->getUrl() : null;
-        //                return sprintf('[%s][%s]', Yii::$app->id, $url);
-        //            },
-        //            'logVars'=>[],
-        //            'logTable'=>'{{%system_log}}'
-        //        ]
-        //    ],
-        //],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                'db'=>[
+                    'class' => 'yii\log\DbTarget',
+                    'levels' => ['error', 'warning'],
+                    'except'=>['yii\web\HttpException:*', 'yii\i18n\I18N\*'],
+                    'prefix'=>function () {
+                        $url = !Yii::$app->request->isConsoleRequest ? Yii::$app->request->getUrl() : null;
+                        return sprintf('[%s][%s]', Yii::$app->id, $url);
+                    },
+                    'logVars'=>[],
+                    'logTable'=>'{{%system_log}}'
+                ]
+            ],
+        ],
         'elasticsearch' => [
             'class' => 'yii\elasticsearch\Connection',
             'nodes' => [
---
+++ console/migrations/m160411_090414_log_init.php
@@ -0,0 +1,7 @@
+<?php
+require(Yii::getAlias('@yii/log/migrations/m141106_185632_log_init.php'));
+
+class m160411_090414_log_init extends m141106_185632_log_init
+{
+
+}
