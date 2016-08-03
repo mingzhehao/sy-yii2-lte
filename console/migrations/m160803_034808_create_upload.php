475f0b2987155da1fc1b4ff683313998740e5ca9
yun.song <yun.song1989@gmail.com>
2016-08-03 12:00:20 +0800

    添加upload 表

---
+++ console/migrations/m160803_034808_create_upload.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+class m160803_034808_create_upload extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%upload}}', [
+            'id' => $this->primaryKey(),
+            'image' => $this->string()->notNull(),
+            'file' => $this->string()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%upload}}');
+    }
+}
